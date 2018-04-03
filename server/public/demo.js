634c9ae57f8df98bf862473df00df3baa9fce96c
Haoran Feng <harrifeng@gmail.com>
2018-04-03 18:49:33 +0800

    ok by now

--- server/public/demo.js
+++ server/public/demo.js
@@ -6,14 +6,16 @@ var app = new Vue({
   },
   methods: {
     getFormValues () {
-      body = {}
+      body = [{
+        'member_id': parseInt(this.$refs.my_input.value)
+      }]
       this.$http.post('member_info', body).then(response => {
-        let body = response.body;
-        console.log(body.code)
-        if (body.code != 0 ) {
+        let ret = response.body;
+        console.log(ret.code)
+        if (ret.code != 0 ) {
           this.output = "Member Not existed"
         } else {
-          this.output = body.Data
+          this.output = ret.Data
         }
       }, response => {
         this.output = 'error return'
