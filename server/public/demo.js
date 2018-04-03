285624e00811f7adbf0c62ff56fd77da9676e17f
Haoran Feng <harrifeng@gmail.com>
2018-04-03 18:18:59 +0800

    new setting

--- server/public/demo.js
+++ server/public/demo.js
@@ -1,6 +1,12 @@
 var app = new Vue({
   el: '#app',
   data: {
+    output: '',
     message: 'Hello World Vue!'
+  },
+  methods: {
+    getFormValues () {
+      this.output = this.$refs.my_input.value
+    }
   }
 })
